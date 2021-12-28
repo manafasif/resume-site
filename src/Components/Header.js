f7a7b0f769dfd83c4d141fc87c24a0c43be351e5
manafasif <76545635+manafasif@users.noreply.github.com>
2021-12-27 23:05:12 -0600

    custom scrollbar implementation started

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -48,7 +48,7 @@ class Header extends Component {
                 <Fade bottom duration={2000}>
                   <ul className="social">
                     <a href={project} className="button btn project-btn">
-                      <i className="fa fa-book"></i>Project
+                      <i className="fa fa-book"></i>Source Code
                     </a>
                     <a href={github} className="button btn github-btn">
                       <i className="fa fa-github"></i>Github
