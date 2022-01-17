225ac6dab5af06b9f766d1448193bd52946d82c1
manafasif <76545635+manafasif@users.noreply.github.com>
2022-01-17 15:02:24 -0600

    fixed header button bug

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -48,10 +48,10 @@ class Header extends Component {
               <hr />
               <Fade bottom duration={2000}>
                 <ul className="social">
-                  <a href={project} className="button btn project-btn">
+                  <a href={project} className="button btn project-btn" target="_blank" rel="noreferrer">
                     <i className="fa fa-book"></i>LinkedIn
                   </a>
-                  <a href={github} className="button btn github-btn">
+                  <a href={github} className="button btn github-btn" target="_blank" rel="noreferrer">
                     <i className="fa fa-github"></i>Github
                   </a>
                 </ul>
